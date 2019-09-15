db60137ce25955d6e36d0d232f25e09f8f3c5b3b
dacre-denny <dacre@mooce.co.nz>
2019-09-15 21:57:11 +1200

    Inital implementation with some tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export const setStateCallback = () => {};
+import * as React from "react";
+
+export const setStateCallback = <T extends any>(
+  initial: T,
+  callback: (state: T) => void
+): [T | undefined, React.Dispatch<React.SetStateAction<T | undefined>>] => {
+  const hasRun = React.useRef(false);
+  const [state, setState] = React.useState<T>(initial);
+
+  React.useEffect(() => {
+    if (hasRun.current) {
+      callback(state);
+    } else {
+      hasRun.current = true;
+    }
+  }, [state]);
+
+  return [state, setState];
+};
--- test/index.spec.tsx
+++ test/index.spec.tsx
@@ -4,28 +4,48 @@ import * as React from "react";
 import * as sinon from "sinon";
 import { setStateCallback } from "../src/index";
 
+interface TestProps<T> {
+  onChange: (arg: T) => void;
+  value: T;
+}
+
+function HookTest<T>(props: TestProps<T>) {
+  const [hookValue, hookSetValue] = setStateCallback<T>(props.value, props.onChange);
+
+  React.useEffect(() => hookSetValue(props.value), [props.value]);
+
+  return <>{hookValue}</>;
+}
+
 describe("The setStateCallback hook", (): void => {
   afterEach((): void => {
     sinon.restore();
   });
 
-  it("some thing", (): void => {
-    setStateCallback();
+  it("Should not invoke state change callback from initial state", (): void => {
+    const callbackSpy = sinon.spy();
+    const wrapper = mount(<HookTest value={5} onChange={callbackSpy} />);
 
-    const warnStub = sinon.stub(console, "warn");
-    const wrapper = mount(
-      <div>
-        <p>test</p>
-      </div>
-    );
+    assert.equal(wrapper.text(), "5");
+    assert.isFalse(callbackSpy.called);
+  });
 
-    assert.isTrue(
-      wrapper.containsMatchingElement(
-        <div>
-          <p>test</p>
-        </div>
-      )
-    );
-    assert.isFalse(warnStub.called);
+  it("Should invoke state change callback for state changes after initial state", (): void => {
+    const callbackSpy = sinon.spy();
+    const wrapper = mount(<HookTest value={5} onChange={callbackSpy} />);
+
+    wrapper.setProps({ value: 10 });
+    wrapper.update();
+
+    assert.equal(wrapper.text(), "10");
+    assert.isTrue(callbackSpy.calledOnce);
+    assert.isTrue(callbackSpy.calledWith(10));
+
+    wrapper.setProps({ value: 15 });
+    wrapper.update();
+
+    assert.equal(wrapper.text(), "15");
+    assert.isTrue(callbackSpy.calledTwice);
+    assert.isTrue(callbackSpy.calledWith(15));
   });
 });
